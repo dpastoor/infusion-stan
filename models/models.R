d3b06a6937fcb77e21f9bee521b6e756639649b9
Devin Pastoor <devin.pastoor@gmail.com>
2017-06-10 17:36:36 +0200

    source model

--- models/models.R
+++ models/models.R
@@ -1,7 +1,7 @@
 library(overseer)
 
 # check to make sure sourcing from proper directory if running interactively
-if (!interactive_model_check("inf-wt.cpp")) {
+if (!interactive_model_check("inf_wt.cpp")) {
     stop("make sure the directory is set to the models directory before running interactively,
     to make sure the relative paths will be the same as when sourcing")
 }
@@ -10,7 +10,7 @@ models <- Overseer$new()
 
 
 # add model files below
-models$add_model_file("inf-wt.cpp")
+models$add_model_file("inf_wt.cpp")
 # return overseer instance to be pulled in via source()$value
 # do not add any code below this line or delete the models object
 # or sourcing the file may not work properly
